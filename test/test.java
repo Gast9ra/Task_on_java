3a7ece78bcb0ba7a8d45b667a2b98f67d3a54383
Gast9ra <Mavrovasili@gmail.com>
2017-09-22 11:49:26 +0300

    test is work

--- test/test.java
+++ test/test.java
@@ -1,5 +1,9 @@
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import pack.LineMas;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class test {
@@ -8,8 +12,9 @@ public class test {
         LineMas first=new LineMas();
         int[] tes = new int[]{12, 435, -20, -2000000000, 300000, 87465};
         int[] mas = new int[]{1,2,3,4,5,6,-25,6};
-        assertEquals(new int[]{4,5},first.maxInMas(tes));
-        assertEquals(new int[]{0,5},first.maxInMas(mas));
+        assertArrayEquals(new int[]{4,5},first.maxInMas(tes));
+        assertArrayEquals(new int[]{0,5},first.maxInMas(mas));
+
     }
 
 
