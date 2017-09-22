e79e3b88a694ab612cc9f69c2bf1ee3ce958ddf5
Gast9ra <Mavrovasili@gmail.com>
2017-09-22 11:56:43 +0300

    test is work

--- test/test.java
+++ test/test.java
@@ -10,10 +10,8 @@ public class test {
     @Test
     void testfirs(){
         LineMas first=new LineMas();
-        int[] tes = new int[]{12, 435, -20, -2000000000, 300000, 87465};
-        int[] mas = new int[]{1,2,3,4,5,6,-25,6};
-        assertArrayEquals(new int[]{4,5},first.maxInMas(tes));
-        assertArrayEquals(new int[]{0,5},first.maxInMas(mas));
+        assertArrayEquals(new int[]{4,5},first.maxInMas(new int[]{12, 435, -20, -2000000000, 300000, 87465}));
+        assertArrayEquals(new int[]{0,5},first.maxInMas(new int[]{1,2,3,4,5,6,-25,6}));
 
     }
 
